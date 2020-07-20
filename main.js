6198965f1cff24e34cac8e40c88ab6a59ee66bf3
liz-barnes <embarnes95@gmail.com>
2020-07-20 18:52:34 -0500

    data type info

--- main.js
+++ main.js
@@ -1,1 +1,78 @@
-console.log("It is Monday.")
+// // undefined - declare or created variable but do not assign //
+
+// let x:
+
+// console.log(x)
+
+// // boolean- true/false
+
+// let x = true;
+// let y = false;
+
+// console.log(x, y)
+
+// // if (x) {
+// //   console.log("This is true")
+// // } else {
+// //   console.log("This is false")
+// // }
+
+// // if (!x) {   // * means not true, so it flips the logic
+// //   console.log("This is true")
+// // } else {
+// //   console.log("This is false")
+// // }
+
+// // // NUmber 
+// // let number = '23'
+
+// // console.log(number * 23) // * 529
+
+// // console.log(number + 23) // * '2323'
+
+// // console.log(Number(number) + 23) // * 46 
+
+
+// // // String 
+
+// // let string = 'Today is Monday!'
+
+// // if (string === 'today is monday') {
+// //   console.log("YAYAYYYY!!")
+// // }
+
+// // console.log(typeof string)
+
+// let array = []
+// let object = {}
+
+// let myFunction = () => { // * ES6
+//   return
+// }
+
+// function thatFunction() { // * ES5
+//   return
+// }
+// console.log(typeof array)
+// console.log(typeof object)
+// console.log(typeof thatFunction)
+
+
+
+let name = 'Liz', // * object shorthand notation
+    age = "23",
+    school = "NSS";
+
+// let object = {
+//   name: name,
+//   age: age,
+//   school: school
+// }
+
+// let object = { // * object shorthand
+//   name,
+//   age,
+//   school
+// }
+
+// console.log(object.age)
